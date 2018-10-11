38dabcb8fea851a379718bb62ab5312b1b2e1deb
Gerard Antoun <gerard.antoun@yahoo.com>
2018-10-11 20:02:50 +0300

    added server listen port 80

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
+// Server settings
+const PORT = 80;
+
 const express = require('express');
 
 const app = express();
 
 app.get('/', (req, res) => {
+    console.log('req: home');
     res.send('Bism illah');
+});
+
+app.listen(PORT, ()=> {
+    console.log(`Server started on port ${PORT}`);
 });
