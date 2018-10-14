29ad285846733c5d0d6c5394ed4561d36f0266e0
Gerard Antoun <gerard.antoun@yahoo.com>
2018-10-14 19:09:46 +0300

    fixing keyboard issue again: added separate overlay div

--- public/js/home.js
+++ public/js/home.js
@@ -1,12 +1,14 @@
 const dForgotPassBtn = _('forgot-pass-btn');
 const dPopup = _('popup');
 const dPopupExitBtn = _('exit-popup');
-
+const dOverlay = _('overlay');
 // Show popup on "forgot password" button click
 dForgotPassBtn.onclick = () => {
     dPopup.style.display = 'block';
+    dOverlay.style.display = 'block';
 };
 
 dPopupExitBtn.onclick= () => {
     dPopup.style.display = 'none';
+    dOverlay.style.display = 'none';
 }
