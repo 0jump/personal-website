390857fb64407ae4180ad6ccf25c1a2596da980b
Gerard Antoun <gerard.antoun@yahoo.com>
2018-10-26 20:15:04 +0300

    added body-parser middle ware

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const config = require('./lib/config');
 const express = require('express');
+const bodyParser = require('body-parser');
 const path = require('path');
 const dbservices = require('./lib/dbservices');
 
@@ -15,6 +16,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// Body Parser Middleware
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
 // Home Route
 app.get('/', (req, res) => {
     res.render('home');
@@ -23,6 +31,11 @@ app.get('/', (req, res) => {
 // Timer Route
 app.get('/timer', (req, res) => {
     res.render('timer');
+});
+
+// AJAX Requests from Client-Side
+app.post('/users', (req, res) => {
+    
 });
 
 app.listen(PORT, ()=> {
