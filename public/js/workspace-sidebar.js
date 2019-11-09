563278aba84cf28b5637f463657c483b7f059eac
Gerard Antoun <gerard.antoun@yahoo.com>
2019-11-09 23:59:54 +0100

    Sidebar: no more coming increase in height of page when sidebar is opened

--- public/js/workspace-sidebar.js
+++ public/js/workspace-sidebar.js
@@ -1,23 +1,35 @@
 /* Ths file should encompass all sidebar functionality for users who are LOGGED IN. Including the opening and closing of the sidebar upon the clicking of the burger icon*/
 
+const subBody = _("sub-body");
 const sidebarCtr = _("sidebar-ctr");
-const header = _("header");
-const mainCtr = _("container");
 const wrapper = _("wrapper");
 const inner = _("inner");
+const header = _("header");
+const mainCtr = _("container");
+
 
 
 // Functionality 1: Opening the sidebar on burger icon click
 let navMenuBtn = _('nav-menu-icon');
 let isSideBarShown = false;
 
-let resizeWrapperToStayResponsiveWhenSidebarIsOpen = () => {
-    
-    if(window.innerWidth <= 450){
+let makeSidebarMobileFriendly = () => {
+    if(window.innerWidth <= 550){
         // if the width of the window is too small,do nothing because decreasing the width of the content is so small that the burger icon will no longer be visible and therefore the sidebar cannot be closed
+        // if the width is too small make sure the wrapper is using the correct css classes so it doesn't hide parts of the screen
+        if(!Array.from(inner.classList).includes("wrapper-sidebar-mobile-friendly")){
+            inner.classList.toggle('wrapper-sidebar-mobile-friendly');
+        }
     }else{
-        wrapper.style.width = `${window.innerWidth - 256}px`;
+        if(Array.from(inner.classList).includes("wrapper-sidebar-mobile-friendly")){
+            inner.classList.toggle('wrapper-sidebar-mobile-friendly');
+        }
+        resizeWrapperToStayResponsiveWhenSidebarIsOpen();
     }
+}
+
+let resizeWrapperToStayResponsiveWhenSidebarIsOpen = () => {
+    wrapper.style.width = `${window.innerWidth - 256}px`;
 }
 
 let showSidebar = () => {
@@ -31,9 +43,9 @@ let showSidebar = () => {
     inner.classList.toggle("inner-moved-right");
 
     // Add an event listener to keep the content that is now sharing the window with the sidebar responsive
-    window.addEventListener("resize", resizeWrapperToStayResponsiveWhenSidebarIsOpen);
+    window.addEventListener("resize", makeSidebarMobileFriendly);
     // Initially resize the content to still be responsive
-    resizeWrapperToStayResponsiveWhenSidebarIsOpen();
+    makeSidebarMobileFriendly();
     isSideBarShown = true;
 }
 
@@ -44,7 +56,7 @@ let hideSideBar = () => {
     }, 500);
     sidebarCtr.classList.toggle("open-sidebar");
     inner.classList.toggle("inner-moved-right");
-    window.removeEventListener("resize", resizeWrapperToStayResponsiveWhenSidebarIsOpen);
+    window.removeEventListener("resize", makeSidebarMobileFriendly);
     wrapper.style.width = "100%";
     isSideBarShown = false;
 }
